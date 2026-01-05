7e02337f2e95553d6cebf7b718a7a1aff0c809fb
alaa eddine guenat <Oa_geunat@esi.dz>
2026-01-05 12:20:34 +0100

    orders schema changes

--- app/db/schema/orders.py
+++ app/db/schema/orders.py
@@ -1,19 +1,20 @@
 from pydantic import BaseModel
-from typing import List,Optional
+from typing import List, Optional
 from datetime import datetime
+from app.db.schema.order_items import OrderItemCreate, OrderItemOut
 
 class OrderBase(BaseModel):
-    user_id:int 
-    status:Optional[str] = 'pending'
+    user_id: int 
+    status: Optional[str] = 'pending'
 
 class OrderCreate(OrderBase):
-    items: List['OrderItemCerate']
+    items: List[OrderItemCreate]
 
 class OrderOut(OrderBase):
-    id:int
-    total_price:float
-    created_at:datetime
-    items: List["OrderItemOut"]
+    id: int
+    total_price: float
+    created_at: datetime
+    items: List[OrderItemOut]
 
     class Config:
         from_attributes = True
